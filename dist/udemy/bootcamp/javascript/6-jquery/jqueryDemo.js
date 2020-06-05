1fd69e04450d413934cde8c618484dbf95b43535
Joe <40449710+sliceofjoe@users.noreply.github.com>
2020-06-05 12:28:17 +1000

    jQuery
    -jQuery Selectors
    -jQuery Methods
    .attr()
    .val()
    .addClass()
    .removeClass()
    .toggleClass()
    -jQuery Events
    .click()
    .keyup()
    .keypress()
    .on()
    -jQuery Effects
    .fadeOut()
    .fadeIn()
    .fadeToggle()
    .slideDown()
    .slideUp()
    .slideToggle()

---
+++ dist/udemy/bootcamp/javascript/6-jquery/jqueryDemo.js
@@ -0,0 +1,10 @@
+//select all divs and give them a purple background
+$("div").css("backgroundColor", "purple");
+
+//select the divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width", "200px");
+//select the div with id "third" and give it an orange border
+$("#third").css("border", "orange 3px dotted");
+
+//Select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
