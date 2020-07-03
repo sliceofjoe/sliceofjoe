f93753d04053113534c36371dd01a650161b3c45
Joe <40449710+sliceofjoe@users.noreply.github.com>
2020-07-03 17:47:39 +1000

    Authentication

---
+++ dist/udemy/bootcamp/backend/Authentication/app.js
@@ -0,0 +1,90 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "J3T Coming for you",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=======================
+//ROUTES
+//=======================
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret", isLoggedIn, function(req, res){
+    res.render("secret");
+});
+
+
+//AUTH ROUTES=============
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+//LOGIN ROUTES=================
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+//LOGOUT ROUTE=====================
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } 
+    res.redirect("/login");
+}
+
+
+// Tell Express to listen for requests (start server)
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("The Server Has Started!");
+});
---
+++ dist/udemy/bootcamp/backend/Authentication/readme.md
@@ -0,0 +1,21 @@
+#Auth Code Part 1
+* Set up folder structure
+* Install needed packages
+* Add root route and template
+* Add secret route and template
+
+#Auth Code Part 2
+* Create user model
+* Configure passport
+
+#Auth Code Part 3
+* Add Register routes
+* Add Register form
+
+#Auth Code Part 4
+* Add Login routes
+* Add Login form
+
+#Auth Code Part 5
+* Add Logout Route
+* Add isLoggedIn middleware
