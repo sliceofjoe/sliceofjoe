f8261df926b358699a10b1f8de54458327410abc
Joe <40449710+sliceofjoe@users.noreply.github.com>
2020-07-08 15:57:22 +1000

    YelpCamp v12
    Landing Page
    -Refeactored layout of landing page
    -Added image and fade animations
    
    Dynamic Price
    -Added user pricing to campground model

---
+++ dist/udemy/bootcamp/backend/YelpCamp/v12/routes/index.js
@@ -0,0 +1,54 @@
+var express  = require("express");
+var router   = express.Router();
+var passport = require("passport");
+var User     = require("../models/user");
+
+router.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+//===========================
+//AUTH ROUTES
+//===========================
+
+//Show register form
+router.get("/register", function(req, res){
+  res.render("register");
+});
+
+//handle signup logic
+router.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function( err, user){
+    if(err){
+      return res.render("register", {"error": err.message});
+    } 
+    passport.authenticate("local")(req, res, function(){
+      req.flash("success", "Welcome to YelpCamp " + user.username);
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
+//Show LOGIN form
+router.get("/login", function(req, res){
+  res.render("login");
+});
+
+//Handling LOGIN logic
+router.post("/login", passport.authenticate("local", 
+  {
+    successRedirect: "/campgrounds", 
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+//LOGOUT route
+router.get("/logout", function(req, res){
+  req.logout();
+  req.flash("success", "Leaving so soon?");
+  res.redirect("/campgrounds");
+});
+
+module.exports = router;
